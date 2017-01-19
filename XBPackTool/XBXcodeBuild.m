f4ed6f3a9619c4d942ec42ca777e28100d3a211c
dym <dym@dymdeimac.local>
2017-01-19 18:30:07 +0800

    1.修改了项目名称的问题

--- XBPackTool/XBXcodeBuild.m
+++ XBPackTool/XBXcodeBuild.m
@@ -29,10 +29,10 @@
         NSString *cd = [NSString stringWithFormat:@"cd %@",XB_ProjectPath];
     
     // 1.清理工程
-        NSString *clean = [NSString stringWithFormat:@"/usr/bin/xcodebuild -workspace ManMao.xcworkspace -scheme %@  clean",XB_ProjectName];
+        NSString *clean = [NSString stringWithFormat:@"/usr/bin/xcodebuild -workspace %@.xcworkspace -scheme %@  clean",XB_ProjectName,XB_ProjectName];
     
     // 2.编译项目
-        NSString *build = [NSString stringWithFormat:@"/usr/bin/xcodebuild -workspace ManMao.xcworkspace -scheme %@ -sdk iphoneos -configuration %@ build" ,XB_ProjectName,buildType];
+        NSString *build = [NSString stringWithFormat:@"/usr/bin/xcodebuild -workspace %@.xcworkspace -scheme %@ -sdk iphoneos -configuration %@ build" ,XB_ProjectName,XB_ProjectName,buildType];
     
         NSString *shell1 = [NSString stringWithFormat:@"%@\n %@\n %@\n",cd,clean,build];
         system([shell1 cStringUsingEncoding:NSUTF8StringEncoding]);
